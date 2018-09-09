070ea5d2fc591ef1f4636fc0a644209403f18303
jd <499576122@qq.com>
2018-09-09 20:00:58 +0800

    增加缓存类

---
+++ cache/cache.go
@@ -0,0 +1,89 @@
+package cache
+
+import (
+	"microSocket/util"
+	"sync"
+	"time"
+)
+
+var (
+	fileCache   sync.Map
+	memoryCache sync.Map
+	fileLock    sync.Mutex
+)
+
+//缓存 数据
+func SaveCache(key, value string, isFile bool) bool {
+	if isFile {
+		_, ok := fileCache.LoadOrStore(key, value)
+		return ok
+	} else {
+		_, ok := memoryCache.LoadOrStore(key, value)
+		return ok
+	}
+
+}
+
+//获取数据
+func GetCache(key string, isFile bool) string {
+	if isFile {
+		v, ok := fileCache.Load(key)
+		if ok {
+			return v.(string)
+		}
+	} else {
+		v, ok := memoryCache.Load(key)
+		if ok {
+			return v.(string)
+		}
+	}
+	return "'"
+}
+
+//删除数据
+func DeleteCache(key string, isFile bool) {
+	if isFile {
+		fileCache.Delete(key)
+	} else {
+		memoryCache.Delete(key)
+	}
+}
+
+//每隔一段时间缓存一次到文件里面
+func saveFile() {
+	saveMap := make(map[string]string)
+
+	fileCache.Range(func(key, value interface{}) bool {
+		saveMap[key.(string)] = value.(string)
+		return true
+	})
+
+	saveStr := util.Map2String(saveMap)
+
+	fileLock.Lock()
+	defer fileLock.Unlock()
+
+	util.WriteFile("./tmp.txt", saveStr)
+}
+
+//在此模块一开始运行的时候加载缓存文件
+func loadFile() {
+	fileLock.Lock()
+	defer fileLock.Unlock()
+
+	fileStr, _ := util.ReadFile("./tmp.txt")
+	loadMap := util.String2Map(fileStr)
+
+	for i, v := range loadMap {
+		fileCache.LoadOrStore(i, v)
+	}
+}
+
+func init() {
+	loadFile()
+
+	go func() {
+		time.Sleep(time.Second)
+		saveFile()
+	}()
+}
